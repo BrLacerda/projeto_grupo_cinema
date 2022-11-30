2d5f31b218f2d8454e6e4bef8981f6d6d4edc635
Akanoshi <113038068+Akanoshi@users.noreply.github.com>
2022-11-29 23:30:12 -0300

    del

--- project_3/src/App.jsx
+++ project_3/src/App.jsx
@@ -1,7 +1,7 @@
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 import { Outlet } from 'react-router-dom';
-import './App.css';
+
 
   function App() {
       return (
